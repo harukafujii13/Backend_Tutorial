4e4d87983c2b9b99657a72cc9a3fd574b03fedd0
Haruka <hfujii1013@gmail.com>
2023-10-28 21:02:01 -0700

    feat: add parallelSequenceaRace

---
+++ Section22/parallelSequenceRace.js
@@ -0,0 +1,11 @@
+const promisify = (item, delay) =>
+  new Promise((resolve) => setTimeout(() => resolve(item), delay));
+
+const a = () => promisify("a", 100);
+const b = () => promisify("b", 5000);
+const c = () => promisify("c", 3000);
+
+async function parallel() {
+  const promise = [a(), b(), c()];
+  const [output1, output2, output3] = await Promise.all;
+}
