9d52a2354d95f47384a73f52fc85996ad872a43d
Haruka <hfujii1013@gmail.com>
2023-11-07 17:06:57 -0800

    delete: memo

--- Section22/allSetted.js
+++ Section22/allSetted.js
@@ -5,6 +5,3 @@ const promiseTwo = new Promise((resolve, reject) => setTimeout(reject, 3000));
 Promise.allSettled([promiseOne, promiseTwo])
   .then((data) => console.log(data))
   .catch((e) => console.log("something failed"), e);
-
-//allSetted
-//allSetted()
