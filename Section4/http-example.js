6bc2712d449959f9cf657bb22e96a3a1467750ec
Haruka <hfujii1013@gmail.com>
2023-11-09 23:57:01 -0800

    feat: add module 39

--- Section4/http-example.js
+++ Section4/http-example.js
@@ -1,1 +1,9 @@
-const http = require("http");
+const { get } = require("https");
+const request = get("https://www.google.com", (res) => {
+  res.on("data", (chunk) => {
+    console.log(`Data chunk: ${chunk}`);
+  });
+  res.on("end", () => {
+    console.log("No more data");
+  });
+});
