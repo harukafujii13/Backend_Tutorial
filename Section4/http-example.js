55db6d7639ad84c1a9f6db962cbd4d54364c6036
Haruka <hfujii1013@gmail.com>
2023-11-11 23:49:14 -0800

    feat: add memo

--- Section4/http-example.js
+++ Section4/http-example.js
@@ -7,3 +7,6 @@ const request = get("https://www.google.com", (res) => {
     console.log("No more data");
   });
 });
+
+//get method from Node.js's built-in https module.
+//The https module is used for making HTTP and HTTPS requests.
